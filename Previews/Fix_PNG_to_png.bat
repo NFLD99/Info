19f10215217ea5a40e2076521e59f33cf5464c92
Dawson <NFLD99@users.noreply.github.com>
2018-05-23 10:14:52 -0230

    Updated
    Added New Themes
    Lambda Orange
    Red Battlefield
    Lu from Elsword
    Persona Five
    Hotline Miami
    Xbox
    Pikachu
    Megadimension Neptunia
    Static
    Static Rainbow

---
+++ Previews/Fix_PNG_to_png.bat
@@ -0,0 +1,159 @@
+@echo off
+echo Renaming .PNG files to .png
+echo ----------
+echo ----------
+echo ----------
+echo ----------
+echo ----------
+echo ----------
+echo ----------
+echo ----------
+echo ----------
+echo ----------
+forfiles /S /M *.PNG /C "cmd /c rename @file @fname.png"
+cls
+echo Renaming .PNG files to .png
+echo ----------
+echo ----------
+echo ----------
+echo ----------
+echo ----------
+echo ----------
+echo ----------
+echo ----------
+echo ----------
+echo ----------
+ping 192.168.0.200 -n 1 -w 500 > nul
+cls
+echo Renaming Please Wait... 10
+echo \---------
+echo \---------
+echo \---------
+echo \---------
+echo \---------
+echo \---------
+echo \---------
+echo \---------
+echo \---------
+echo \---------
+ping 192.168.0.200 -n 1 -w 500 > nul
+cls
+echo Renaming Please Wait... 20
+echo \\--------
+echo \\--------
+echo \\--------
+echo \\--------
+echo \\--------
+echo \\--------
+echo \\--------
+echo \\--------
+echo \\--------
+echo \\--------
+ping 192.168.0.200 -n 1 -w 500 > nul
+cls
+echo Renaming Please Wait... 30
+echo \\\-------
+echo \\\-------
+echo \\\-------
+echo \\\-------
+echo \\\-------
+echo \\\-------
+echo \\\-------
+echo \\\-------
+echo \\\-------
+echo \\\-------
+ping 192.168.0.200 -n 1 -w 500 > nul
+cls
+echo Renaming Please Wait... 40
+echo \\\\------
+echo \\\\------
+echo \\\\------
+echo \\\\------
+echo \\\\------
+echo \\\\------
+echo \\\\------
+echo \\\\------
+echo \\\\------
+echo \\\\------
+ping 192.168.0.200 -n 1 -w 500 > nul
+cls
+echo Renaming Please Wait... 50
+echo \\\\\-----
+echo \\\\\-----
+echo \\\\\-----
+echo \\\\\-----
+echo \\\\\-----
+echo \\\\\-----
+echo \\\\\-----
+echo \\\\\-----
+echo \\\\\-----
+echo \\\\\-----
+ping 192.168.0.200 -n 1 -w 500 > nul
+cls
+echo Renaming Please Wait... 60
+echo \\\\\\----
+echo \\\\\\----
+echo \\\\\\----
+echo \\\\\\----
+echo \\\\\\----
+echo \\\\\\----
+echo \\\\\\----
+echo \\\\\\----
+echo \\\\\\----
+echo \\\\\\----
+ping 192.168.0.200 -n 1 -w 500 > nul
+cls
+echo Renaming Please Wait... 70
+echo \\\\\\\---
+echo \\\\\\\---
+echo \\\\\\\---
+echo \\\\\\\---
+echo \\\\\\\---
+echo \\\\\\\---
+echo \\\\\\\---
+echo \\\\\\\---
+echo \\\\\\\---
+echo \\\\\\\---
+ping 192.168.0.200 -n 1 -w 500 > nul
+cls
+echo Renaming Please Wait... 80
+echo \\\\\\\\--
+echo \\\\\\\\--
+echo \\\\\\\\--
+echo \\\\\\\\--
+echo \\\\\\\\--
+echo \\\\\\\\--
+echo \\\\\\\\--
+echo \\\\\\\\--
+echo \\\\\\\\--
+echo \\\\\\\\--
+ping 192.168.0.200 -n 1 -w 500 > nul
+cls
+echo Renaming Please Wait... 90
+echo \\\\\\\\\-
+echo \\\\\\\\\-
+echo \\\\\\\\\-
+echo \\\\\\\\\-
+echo \\\\\\\\\-
+echo \\\\\\\\\-
+echo \\\\\\\\\-
+echo \\\\\\\\\-
+echo \\\\\\\\\-
+echo \\\\\\\\\-
+ping 192.168.0.200 -n 1 -w 500 > nul
+cls
+echo Renaming Please Wait... 100
+echo \\\\\\\\\\
+echo \\\\\\\\\\
+echo \\\\\\\\\\
+echo \\\\\\\\\\
+echo \\\\\\\\\\
+echo \\\\\\\\\\
+echo \\\\\\\\\\
+echo \\\\\\\\\\
+echo \\\\\\\\\\
+echo \\\\\\\\\\
+ping 192.168.0.200 -n 1 -w 500 > nul
+cls
+echo done
+pause
