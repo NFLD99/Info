2b9c38e302cb5f5eeab630038f116cc4b71008ac
Dawson <NFLD99@users.noreply.github.com>
2018-08-07 01:53:10 -0230

    Updated
    new Themes

---
+++ Previews/Theme_List.bat
@@ -0,0 +1,2 @@
+cd C:\Users\Dawso\AppData\Roaming\BetterDiscord\themes\info\Previews\
+for /D %%A IN ("*") DO echo "%%A">>C:\Users\Dawso\AppData\Roaming\BetterDiscord\themes\info\Previews\Theme_List.txt
