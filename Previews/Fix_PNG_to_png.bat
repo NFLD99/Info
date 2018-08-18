f029904f4c3519c973e7a4c6f58cc9dca4a6dec8
Dawson <NFLD99@users.noreply.github.com>
2018-08-18 04:53:21 -0230

    slient update for me and only me

--- Previews/Fix_PNG_to_png.bat
+++ Previews/Fix_PNG_to_png.bat
@@ -1,159 +1,62 @@
 @echo off
-echo Renaming .PNG files to .png
-echo ----------
-echo ----------
-echo ----------
-echo ----------
-echo ----------
-echo ----------
-echo ----------
-echo ----------
-echo ----------
-echo ----------
 forfiles /S /M *.PNG /C "cmd /c rename @file @fname.png"
 cls
 echo Renaming .PNG files to .png
-echo ----------
-echo ----------
-echo ----------
-echo ----------
-echo ----------
-echo ----------
-echo ----------
-echo ----------
-echo ----------
-echo ----------
-ping 192.168.0.200 -n 1 -w 500 > nul
+echo [----------]
+type NFLD99.txt
+@TIMEOUT /T 1 > NUL
 cls
 echo Renaming Please Wait... 10
-echo \---------
-echo \---------
-echo \---------
-echo \---------
-echo \---------
-echo \---------
-echo \---------
-echo \---------
-echo \---------
-echo \---------
-ping 192.168.0.200 -n 1 -w 500 > nul
+echo [#---------]
+type NFLD99.txt
+@TIMEOUT /T 1 > NUL
 cls
 echo Renaming Please Wait... 20
-echo \\--------
-echo \\--------
-echo \\--------
-echo \\--------
-echo \\--------
-echo \\--------
-echo \\--------
-echo \\--------
-echo \\--------
-echo \\--------
-ping 192.168.0.200 -n 1 -w 500 > nul
+echo [##--------]
+type NFLD99.txt
+@TIMEOUT /T 1 > NUL
 cls
 echo Renaming Please Wait... 30
-echo \\\-------
-echo \\\-------
-echo \\\-------
-echo \\\-------
-echo \\\-------
-echo \\\-------
-echo \\\-------
-echo \\\-------
-echo \\\-------
-echo \\\-------
-ping 192.168.0.200 -n 1 -w 500 > nul
+echo [###-------]
+type NFLD99.txt
+@TIMEOUT /T 1 > NUL
 cls
 echo Renaming Please Wait... 40
-echo \\\\------
-echo \\\\------
-echo \\\\------
-echo \\\\------
-echo \\\\------
-echo \\\\------
-echo \\\\------
-echo \\\\------
-echo \\\\------
-echo \\\\------
-ping 192.168.0.200 -n 1 -w 500 > nul
+echo [####------]
+type NFLD99.txt
+@TIMEOUT /T 1 > NUL
 cls
 echo Renaming Please Wait... 50
-echo \\\\\-----
-echo \\\\\-----
-echo \\\\\-----
-echo \\\\\-----
-echo \\\\\-----
-echo \\\\\-----
-echo \\\\\-----
-echo \\\\\-----
-echo \\\\\-----
-echo \\\\\-----
-ping 192.168.0.200 -n 1 -w 500 > nul
+echo [#####-----]
+type NFLD99.txt
+@TIMEOUT /T 1 > NUL
 cls
 echo Renaming Please Wait... 60
-echo \\\\\\----
-echo \\\\\\----
-echo \\\\\\----
-echo \\\\\\----
-echo \\\\\\----
-echo \\\\\\----
-echo \\\\\\----
-echo \\\\\\----
-echo \\\\\\----
-echo \\\\\\----
-ping 192.168.0.200 -n 1 -w 500 > nul
+echo [######----]
+type NFLD99.txt
+@TIMEOUT /T 1 > NUL
 cls
 echo Renaming Please Wait... 70
-echo \\\\\\\---
-echo \\\\\\\---
-echo \\\\\\\---
-echo \\\\\\\---
-echo \\\\\\\---
-echo \\\\\\\---
-echo \\\\\\\---
-echo \\\\\\\---
-echo \\\\\\\---
-echo \\\\\\\---
-ping 192.168.0.200 -n 1 -w 500 > nul
+echo [#######---]
+type NFLD99.txt
+@TIMEOUT /T 1 > NUL
 cls
 echo Renaming Please Wait... 80
-echo \\\\\\\\--
-echo \\\\\\\\--
-echo \\\\\\\\--
-echo \\\\\\\\--
-echo \\\\\\\\--
-echo \\\\\\\\--
-echo \\\\\\\\--
-echo \\\\\\\\--
-echo \\\\\\\\--
-echo \\\\\\\\--
-ping 192.168.0.200 -n 1 -w 500 > nul
+echo [########--]
+type NFLD99.txt
+@TIMEOUT /T 1 > NUL
 cls
 echo Renaming Please Wait... 90
-echo \\\\\\\\\-
-echo \\\\\\\\\-
-echo \\\\\\\\\-
-echo \\\\\\\\\-
-echo \\\\\\\\\-
-echo \\\\\\\\\-
-echo \\\\\\\\\-
-echo \\\\\\\\\-
-echo \\\\\\\\\-
-echo \\\\\\\\\-
-ping 192.168.0.200 -n 1 -w 500 > nul
+echo [#########-]
+type NFLD99.txt
+@TIMEOUT /T 1 > NUL
 cls
 echo Renaming Please Wait... 100
-echo \\\\\\\\\\
-echo \\\\\\\\\\
-echo \\\\\\\\\\
-echo \\\\\\\\\\
-echo \\\\\\\\\\
-echo \\\\\\\\\\
-echo \\\\\\\\\\
-echo \\\\\\\\\\
-echo \\\\\\\\\\
-echo \\\\\\\\\\
-ping 192.168.0.200 -n 1 -w 500 > nul
+echo [##########]
+type NFLD99.txt
+@TIMEOUT /T 1 > NUL
 cls
-echo done
-pause
+echo Finished.
+echo Window will close in 10s, Or press any key to close now.
+type NFLD99.txt
+@TIMEOUT /T 10 > NUL
